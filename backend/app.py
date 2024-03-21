2409a0a1c13d9f4d0e89361cafe5b46fe6adfe99
Aya Al Baba <ayababa12@outlook.com>
2024-03-21 11:53:18 +0200

    added routes to post managers and employees

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,13 @@ from flask import Flask
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask import request
+from flask import jsonify
 from flask_marshmallow import Marshmallow
 import datetime
+import re
+from .model.manager import Manager , manager_schema
+from .model.employee import Employee , employee_schema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///skillsphere.db'
@@ -11,3 +16,36 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 ma = Marshmallow(app)
 
+def is_valid_email(email):
+    # Regular expression for email validation
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
+
+@app.route('/createManager',methods=['POST'])
+def createManager():
+    if request.json["email"]=='' or request.json["password"]=='' or not is_valid_email(request.json["email"]):
+        return jsonify({'message': 'bad email or password'}),400
+    try:
+        m= Manager(request.json["email"], request.json["password"], request.json["first_name"], request.json["last_name"]) 
+        db.session.add(m) 
+        db.session.commit()
+    except:
+        return jsonify({'message': 'email already exists'}),400
+     
+    return jsonify(manager_schema.dump(m)) , 201
+
+@app.route('/createEmployee', methods=['POST'])
+def createEmployee():
+    if request.json["email"]=='' or request.json["password"]=='' or not is_valid_email(request.json["email"]):
+        return jsonify({'message': 'bad email or password'}),400
+    if request.json["gender"] not in ['Male', 'Female']:
+        return jsonify({'message': 'bad gender'}),400
+    try:
+        e= Employee(request.json["email"], request.json["password"], request.json["first_name"], request.json["last_name"],request.json["department"], request.json['gender'], request.json['date_of_birth'])
+
+        db.session.add(e) 
+        db.session.commit()
+    except:
+        return jsonify({'message': 'email already exists'}),400
+     
+    return jsonify(employee_schema.dump(e)) , 201
