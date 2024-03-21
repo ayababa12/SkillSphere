af36633e8bd2364d96b99a79131480a808464485
Aya Al Baba <ayababa12@outlook.com>
2024-03-21 13:10:31 +0200

    basic setup for homepage and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-//import Navbar from "./components/Navbar"; //////
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getUserToken, saveUserToken, clearUserToken } from "./localStorage";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+import LoginPage from './pages/login';
+import HomePage from './pages/home';
 
+var SERVER_URL = "http://127.0.0.1:5000" 
 
 function App() {
+
+  let [userToken, setUserToken] = useState(getUserToken()); 
+
   return (
     <Router>
       <Routes>
@@ -18,7 +21,7 @@ function App() {
         />
         <Route
           path="/"
-          element={token ? <HomePage /> : <Navigate to="/login" replace />}
+          element={userToken ? <HomePage /> : <Navigate to="/login" replace />}
         />
       </Routes>
     </Router>
