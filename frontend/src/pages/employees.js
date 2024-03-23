ffad2b8f39c17ece02a14efdd0138019c3d24e96
Aya Al Baba <ayababa12@outlook.com>
2024-03-23 10:09:08 +0200

    finished displaying employees in table

--- frontend/src/pages/employees.js
+++ frontend/src/pages/employees.js
@@ -13,6 +13,14 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import {SERVER_URL} from '../App'
 import Navigation from '../components/navigation';
 import { useCallback } from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
 
 
@@ -168,7 +176,9 @@ const DisplayEmployeePage = ({isManager, userToken}) => {
     const navigate = useNavigate ();
 
     let [employeeList, setEmployeeList] = useState([]);
-    
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+
     const fetchEmployeeList = useCallback(() => { 
         fetch(`${SERVER_URL}/employees`, { 
           headers: { 
@@ -187,6 +197,35 @@ const DisplayEmployeePage = ({isManager, userToken}) => {
     } 
     }, [isManager, userToken]); 
      
+    const columns = [
+        { id: 'first_name', label: 'First Name', minWidth: 170 },
+        { id: 'last_name', label: 'Last Name', minWidth: 100 },
+        {
+          id: 'department',
+          label: 'Department',
+          minWidth: 170,
+          align: 'right',
+          format: (value) => value.toLocaleString('en-US'),
+        },
+        {
+          id: 'email',
+          label: 'Email',
+          minWidth: 170,
+          align: 'right',
+          format: (value) => value.toLocaleString('en-US'),
+        }
+      ];
+
+      
+    
+      const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+      };
+    
+      const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+      };
 
     return(
         <div>
@@ -198,6 +237,54 @@ const DisplayEmployeePage = ({isManager, userToken}) => {
                 > 
                 Add New Employee
             </Button>
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                <TableContainer sx={{ maxHeight: 440 }}>
+                    <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                        {columns.map((column) => (
+                            <TableCell
+                            key={column.id}
+                            align={column.align}
+                            style={{ minWidth: column.minWidth }}
+                            >
+                            {column.label}
+                            </TableCell>
+                        ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {employeeList
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row) => {
+                            return (
+                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                {columns.map((column) => {
+                                const value = row[column.id];
+                                return (
+                                    <TableCell key={column.id} align={column.align}>
+                                    {column.format && typeof value === 'number'
+                                        ? column.format(value)
+                                        : value}
+                                    </TableCell>
+                                );
+                                })}
+                            </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                    count={employeeList.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
         </div>
     );
 };
