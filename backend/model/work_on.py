0ffbfb3bf588511f36e05b2d84a34971fa0b8d91
Saad-Y <swy03@mail.aub.edu>
2024-03-24 12:59:40 +0200

    Fixed datetime

--- backend/model/work_on.py
+++ backend/model/work_on.py
@@ -5,6 +5,7 @@ from sqlalchemy import Enum
 class WorkOn(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subtask_id = db.Column(db.Integer, db.ForeignKey('subtask.id'), nullable=False)
-    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    employee_email = db.Column(db.Integer, db.ForeignKey('employee.email'), nullable=False)
     start_time = db.Column(db.DateTime, default=datetime.utcnow)
     end_time = db.Column(db.DateTime, nullable=True)
+    is_completed = db.Column(db.Boolean, default=False, nullable=False)  
