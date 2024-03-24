30cd975008cfab06b38c180d70366693a6601517
Saad-Y <swy03@mail.aub.edu>
2024-03-24 13:19:55 +0200

    For subtasks that are taken

--- backend/model/work_on.py
+++ backend/model/work_on.py
@@ -1,11 +1,13 @@
 from ..app import db, datetime, bcrypt, ma
 from sqlalchemy import Enum
+from datetime import datetime,timezone
+
 
 
 class WorkOn(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subtask_id = db.Column(db.Integer, db.ForeignKey('subtask.id'), nullable=False)
     employee_email = db.Column(db.Integer, db.ForeignKey('employee.email'), nullable=False)
-    start_time = db.Column(db.DateTime, default=datetime.utcnow)
+    start_time = db.Column(db.DateTime, default=datetime.now())
     end_time = db.Column(db.DateTime, nullable=True)
     is_completed = db.Column(db.Boolean, default=False, nullable=False)  
