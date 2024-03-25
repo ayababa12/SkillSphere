e04eeb910c9842946378f8319abfc6255ac0a739
Saad-Y <swy03@mail.aub.edu>
2024-03-25 12:00:00 +0200

    Added description field

--- backend/model/subtask.py
+++ backend/model/subtask.py
@@ -1,10 +1,12 @@
 from ..app import db, datetime, bcrypt, ma
 from sqlalchemy import Enum
 
+
 class Subtask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
     title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True) 
     hours = db.Column(db.Integer, nullable=True)
     deadline = db.Column(db.DateTime, nullable=True)
     is_completed = db.Column(db.Boolean, default=False)
