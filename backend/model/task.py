ac7a1e8ee8653916d03811d7fa8dcb2e4695b674
Saad-Y <swy03@mail.aub.edu>
2024-04-12 14:13:24 +0300

    adjusted required fields

--- backend/model/task.py
+++ backend/model/task.py
@@ -1,10 +1,20 @@
+# from ..app import db, datetime, bcrypt, ma
+# from sqlalchemy import Enum
+
+# class Task(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     description = db.Column(db.Text, nullable=True)
+#     deadline = db.Column(db.DateTime, nullable=True)
+#     is_completed = db.Column(db.Boolean, default=False)
+#     subtasks = db.relationship('Subtask', backref='task', lazy='dynamic')
 from ..app import db, datetime, bcrypt, ma
 from sqlalchemy import Enum
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    deadline = db.Column(db.DateTime, nullable=True)
+    description = db.Column(db.Text, nullable=False)  # Changed to required
+    deadline = db.Column(db.DateTime, nullable=False)  # Changed to required
     is_completed = db.Column(db.Boolean, default=False)
     subtasks = db.relationship('Subtask', backref='task', lazy='dynamic')
