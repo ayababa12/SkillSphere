1faa0560f9562303a7a31a1792534e2bdb019648
Saad-Y <swy03@mail.aub.edu>
2024-04-12 22:58:08 +0300

    fixed task

--- backend/model/task.py
+++ backend/model/task.py
@@ -1,13 +1,3 @@
-# from ..app import db, datetime, bcrypt, ma
-# from sqlalchemy import Enum
-
-# class Task(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(100), nullable=False)
-#     description = db.Column(db.Text, nullable=True)
-#     deadline = db.Column(db.DateTime, nullable=True)
-#     is_completed = db.Column(db.Boolean, default=False)
-#     subtasks = db.relationship('Subtask', backref='task', lazy='dynamic')
 from ..app import db, datetime, bcrypt, ma
 from sqlalchemy import Enum
 
